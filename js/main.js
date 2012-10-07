5e56689961a0f0de9f49cbd63b17d1409290bd71
Jon Paul <shikaga@gmail.com>
2012-10-07 13:20:04 +0100

    Okay, how about this for a usecase?

--- js/main.js
+++ js/main.js
@@ -53,21 +53,34 @@ var TopBarHandler = function(element)
 	this.topBar = element;
 }
 
+TopBarHandler.prototype.addTweets = function(tweets)
+{
+	this.tweets = tweets;
+}
+
 TopBarHandler.prototype.renderImages = function(number)
 {
 	this.topBar.innerHTML = "";
 	for(var i=0; i < number; i++)
 	{
-		this.topBar.appendChild(getImage());
+		this.topBar.appendChild(this.tweets[i].smallImage);
 	}
 }
 
 /**
 * This class handles all the tweets in the scroll menu
 */
-var TweetHandler = function()
+var TweetHandler = function(element)
 {
+	this.tweetBar = element;
+}
 
+TweetHandler.prototype.addTweets = function(tweets)
+{
+	for (tweetId in tweets)
+	{
+		this.tweetBar.appendChild(tweets[tweetId].largeImage);
+	}
 }
 
 /**
@@ -78,23 +91,66 @@ var BottomBarHandler = function(element)
 	this.bottomBar = element;
 }
 
+BottomBarHandler.prototype.addTweets = function(tweets)
+{
+	this.tweets = tweets;
+}
+
 BottomBarHandler.prototype.renderImages = function(number)
 {
 	this.bottomBar.innerHTML = "";
-	for(var i=0; i < number; i++)
+	var startIndex = this.tweets.length - number;
+	for(var i=startIndex; i < startIndex+number; i++)
 	{
-		this.bottomBar.appendChild(getImage());
+		this.bottomBar.appendChild(this.tweets[i].smallImage);
 	}
 }
 
-function getImage()
+var Tweet = function(fx)
+{
+	this.smallImage = getSmallImage(fx);
+	this.largeImage = getLargeImage(fx);
+}
+
+function getSmallImage(fx)
 {
 	var image = document.createElement("span");
+	image.innerHTML=fx.substr(0,3) + "<br />" + fx.substr(3)
 	image.style["height"] = "20px";
 	image.style["width"] = "20px";
 	image.style["display"] = "inline-block";
+	image.style["color"] = "white";
+	image.style["font-size"] = "8px";
 	image.style["background-color"] = "black";
 	image.style["margin"] = "5px";
+	return image;
+}
+
+
+function getLargeImage(fx)
+{
+	var image = document.createElement("div");
+	image.style["height"] = "60px";
+	image.style["border"] = "1px solid black";
+	
+	var buyButton = document.createElement("button");
+	buyButton.innerHTML = "BUY!";
+	buyButton.style["height"] = "50px";
+	buyButton.style["width"] = "50px";
+	image.appendChild(buyButton);
+	
+	var currencyPair = document.createElement("span");
+	currencyPair.innerHTML = fx;
+	currencyPair.style["height"] = "50px";
+	currencyPair.style["width"] = "50px";
+	image.appendChild(currencyPair);
+	
+	var sellButton = document.createElement("button");
+	sellButton.innerHTML = "SELL!";
+	sellButton.style["height"] = "50px";
+	sellButton.style["width"] = "50px";
+	image.appendChild(sellButton);
+	
 	return image;
 }
 
